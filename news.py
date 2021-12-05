25dae842855bd24e206e87109009fe766e7a3e4e
Charles Taylor <charlestaylor95@gmail.com>
2021-12-05 11:27:18 -0500

    Rewrote charset

--- news.py
+++ news.py
@@ -1,5 +1,4 @@
-from turtles.alphabet import write as old_write
-from turtles.new_alphabet import write as new_write
+from turtles.new_alphabet import write
 from turtles.utils import new_turtle
 
 from turtle import mainloop, Screen
@@ -7,7 +6,10 @@ from turtle import mainloop, Screen
 
 screen = Screen()
 screen.setup(width=1.0, height=1.0, startx=None, starty=None)
-t = new_turtle(speed=0, pen_size=3)
 
-new_write(t, 'the quick brown fox!'.upper().split())
+write(
+    new_turtle(speed=0, pen_size=3),
+    'the quick brown fox\n jumped over the lazy dog!'.upper().split("\n")
+)
+
 mainloop()
