34b780367209c39331b389aa4149ca0a4bde26d1
Charles Taylor <charlestaylor95@gmail.com>
2021-12-04 19:24:13 -0500

    Refactor clock

--- clock.py
+++ clock.py
@@ -1,9 +1,24 @@
 from datetime import datetime
+from dataclasses import dataclass
 from turtle import Turtle, getscreen, ontimer, mainloop
+
 
 # init
 screen = getscreen()
 screen.bgcolor('black')
+
+radius = 200
+hour_hand_length = radius * 3 / 4
+
+
+@dataclass
+class TurtleHand:
+    turtle: Turtle
+    undo_offset: int
+
+    def reset(self) -> None:
+        while self.turtle.undobufferentries() - self.undo_offset:
+            self.turtle.undo()
 
 
 def make_turtle(speed: int) -> Turtle:
@@ -16,18 +31,38 @@ def make_turtle(speed: int) -> Turtle:
     return t
 
 
+def redraw_hour_hand(hand: TurtleHand, seconds: int) -> None:
+    hand.reset()
+    hours = seconds // 3600
+    t_h = hand.turtle
+    t_h.showturtle()
+    t_h.setheading(90 - 30 * hours)
+    t_h.forward(hour_hand_length)
+    t_h.penup()
+    t_h.forward(2 * radius - hour_hand_length)
+    t_h.hideturtle()
+
+
+# hand functions
+def redraw_minute_hand(hand: TurtleHand, seconds: int) -> None:
+    minutes = seconds // 60
+    hand.reset()
+    turtle = hand.turtle
+    turtle.showturtle()
+    turtle.setheading(90 - 6 * minutes)
+    turtle.forward(radius)
+    turtle.penup()
+    turtle.forward(radius)
+    turtle.hideturtle()
+
 
 def clock() -> None:
-    radius = 200
-    hour_hand_length = radius * 3 / 4
-
     # get current time
     time = datetime.now()
     s = time.second
     m = time.minute
     h = time.hour
     seconds = s + 60 * (m + 60 * h)
-    print(f'{h=}, {m=}, {s=}, {seconds=}')
 
     # seconds hand
     t_s = make_turtle(9)
@@ -44,53 +79,27 @@ def clock() -> None:
     t_s.setheading(90 - 6 * s)
 
     # hours hand
-    t_h = t_s.clone()
-    t_h.hideturtle()
-    t_h.speed(1)
-    t_h_undo_count = t_h.undobufferentries()
-
-    def redraw_hour_hand() -> None:
-        hours = seconds // 3600
-        while t_h.undobufferentries() - t_h_undo_count:
-            t_h.undo()
-        t_h.showturtle()
-        print(hours)
-        t_h.setheading(90 - 30 * hours)
-        t_h.forward(hour_hand_length)
-        t_h.penup()
-        t_h.forward(2 * radius - hour_hand_length)
-        t_h.hideturtle()
-
-    redraw_hour_hand()
+    copy = t_s.clone()
+    copy.hideturtle()
+    copy.speed(1)
+    t_h = TurtleHand(turtle=copy, undo_offset=copy.undobufferentries())
+    redraw_hour_hand(t_h, seconds)
 
     # minutes hand
-    t_m = t_s.clone()
-    t_m.hideturtle()
-    t_m.speed(1)
-    t_m_undo_count = t_m.undobufferentries()
-
-    # hand functions
-    def redraw_minute_hand() -> None:
-        minutes = seconds // 60
-        while t_m.undobufferentries() - t_m_undo_count:
-            t_m.undo()
-        t_m.showturtle()
-        t_m.setheading(90 - 6 * minutes)
-        t_m.forward(radius)
-        t_m.penup()
-        t_m.forward(radius)
-        t_m.hideturtle()
-
-    redraw_minute_hand()
+    copy = t_s.clone()
+    copy.hideturtle()
+    copy.speed(1)
+    t_m = TurtleHand(turtle=copy, undo_offset=copy.undobufferentries())
+    redraw_minute_hand(t_m, seconds)
 
     def tick() -> None:
         nonlocal seconds
         seconds += 1
         if seconds % 3600 == 0:
-            redraw_hour_hand()
+            redraw_hour_hand(t_h, seconds)
 
         if seconds % 60 == 0:
-            redraw_minute_hand()
+            redraw_minute_hand(t_m, seconds)
 
         t_s.right(6)
         ontimer(tick, 100)
