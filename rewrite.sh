af16230ac70886ecc405815d6b51ce9ef9e7085a
Charles Taylor <charlestaylor95@gmail.com>
2021-12-05 11:03:22 -0500

    Update rewrite

--- rewrite.sh
+++ rewrite.sh
@@ -1,9 +1,10 @@
-module="turtles/charset.py"
-rm -f $module 
+module="turtles/charset"
+rm -f "$module.*"
 ./run.sh rewrite 'temp' 
 temp=`cat temp`
-gsed -i -r "s/@/$temp/" $module 
-black $module 
-gsed -i -r "s/\"(Path[^\"]*)\"/\1/g" $module 
-gsed -i -r "s/\"(.*)\"/'\1'/g" $module 
+gsed -i -r "s/@/$temp/" "$module.template"
+mv "$module.template" "$module.py"
+black "$module.py"
+gsed -i -r "s/\"(Path[^\"]*)\"/\1/g" "$module.py"
+gsed -i -r "s/\"(.*)\"/'\1'/g" "$module.py"
 ./run.sh news
