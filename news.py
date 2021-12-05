e608e25e0a7e6adc1c08184a35a3176e324fd90b
Charles Taylor <charlestaylor95@gmail.com>
2021-12-04 22:30:27 -0500

    B, Q

--- news.py
+++ news.py
@@ -2,10 +2,5 @@ from turtles.news import TurtleNews
 
 
 turtle_news = TurtleNews()
-turtle_news.publish([
-    'XYMNLFEHITZ',
-    'JOUDSCGWV',
-    #'ABKPQRY',
-])
-
+turtle_news.publish('THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG'.split())
 turtle_news.start()
